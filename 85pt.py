80c19b01e797721830141735dc6eab15320e0426
rnyorke <rnyorke@gmail.com>
2014-11-05 15:13:01 -0600

    Added movement and Right button

--- 85pt.py
+++ 85pt.py
@@ -28,11 +28,14 @@ class MyApp:
 		self.button1 = Button(self.myContainer1)
 		self.button1.configure(text="Left", background= "green")
 		self.button1.grid(row=0,column=0)
-		
 	
+	        self.button2 = Button(self.myContainer1)
+	        self.button2.configure(text = "Right", background = "green")
+	        self.button2.grid(row=0,column=1)
+	        
 		# "Bind" an action to the first button												
 		self.button1.bind("<Button-1>", self.button1Click)
-		 
+		self.button2.bind("<Button-1>", self.button2Click)
 		  
 		# This creates the drawpad - no need to change this 
 		drawpad.pack()
@@ -47,7 +50,16 @@ class MyApp:
 		global drawpad
 		global drawpadwidth
 		global drawpadheight
-	
+	        drawpad.move(oval,-10,0)
+	def button2Click(self, event):   
+		# Make the oval move to the left!
+                # "global" makes sure that we can access our oval and our drawpad
+                # Add in boundary detection
+		global oval
+		global drawpad
+		global drawpadwidth
+		global drawpadheight
+	        drawpad.move(oval,10,0)
 	# Add the button2Click method
 		
 myapp = MyApp(root)

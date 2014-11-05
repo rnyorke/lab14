157c9dd9130cb5b40db292322f7c48984ced2f63
rnyorke <rnyorke@gmail.com>
2014-11-05 15:38:17 -0600

    Made buttons and movement of player

--- 100pt.py
+++ 100pt.py
@@ -32,12 +32,25 @@ class MyApp:
 		
 		self.button1 = Button(self.myContainer1)
 		self.button1.configure(text="Up", background= "green")
-		self.button1.grid(row=0,column=0)
-					
+		self.button1.grid(row=0,column=1)
+		
+		self.button2 = Button(self.myContainer1)
+		self.button2.configure(text="Left", background= "green")
+		self.button2.grid(row=2,column=0)			
+				
+		self.button3 = Button(self.myContainer1)
+		self.button3.configure(text="Down", background= "green")
+		self.button3.grid(row=2,column=1)
+		
+		self.button4 = Button(self.myContainer1)
+		self.button4.configure(text="RIght", background= "green")
+		self.button4.grid(row=2,column=2)	
+									
 		# "Bind" an action to the first button												
 		self.button1.bind("<Button-1>", self.button1Click)
-
-		  
+		self.button2.bind("<Button-1>", self.button2Click)
+		self.button3.bind("<Button-1>", self.button3Click)
+		self.button4.bind("<Button-1>", self.button4Click)				  
 		# This creates the drawpad - no need to change this 
 		drawpad.pack()
 		
@@ -48,7 +61,74 @@ class MyApp:
 		global oval
 		global drawpad
                 x1,y1,x2,y2 = drawpad.coords(player)
+                drawpad.move(player,0,-10)
+                
 		# Get the coords of our target
+		tx1,ty1,ty1,ty2 = drawpad.coords(target)
+
+
+
+		# Ensure that we are doing our collision detection
+		# After we move our object!
+                didWeHit = collisionDetect()
+                if(didWeHit == True):
+                    # We made contact! Stop our animation!
+                    print "Do something"
+                    
+	def button2Click(self, event):   
+                # "global" makes sure that we can access our oval and our drawpad
+		global oval
+		global drawpad
+                x1,y1,x2,y2 = drawpad.coords(player)
+                drawpad.move(player,-10,0)
+                
+		# Get the coords of our target
+		tx1,ty1,ty1,ty2 = drawpad.coords(target)
+
+
+
+		# Ensure that we are doing our collision detection
+		# After we move our object!
+                didWeHit = collisionDetect()
+                if(didWeHit == True):
+                    # We made contact! Stop our animation!
+                    print "Do something"
+                    
+                    
+                    
+                    
+                    
+        def button3Click(self, event):   
+                # "global" makes sure that we can access our oval and our drawpad
+		global oval
+		global drawpad
+                x1,y1,x2,y2 = drawpad.coords(player)
+                drawpad.move(player,0,10)
+                
+		# Get the coords of our target
+		tx1,ty1,ty1,ty2 = drawpad.coords(target)
+
+
+
+		# Ensure that we are doing our collision detection
+		# After we move our object!
+                didWeHit = collisionDetect(target)
+                if(didWeHit == True):
+                    # We made contact! Stop our animation!
+                    print "Do something"
+                    
+                    
+                    
+	def button4Click(self, event):   
+                # "global" makes sure that we can access our oval and our drawpad
+		global oval
+		global drawpad
+                x1,y1,x2,y2 = drawpad.coords(player)
+                drawpad.move(player,10,0)
+                
+		# Get the coords of our target
+		tx1,ty1,ty1,ty2 = drawpad.coords(target)
+
 
 
 		# Ensure that we are doing our collision detection
